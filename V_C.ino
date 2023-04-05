2c38055fb66551565dc77f9938e093c6b4a2c547
TDD66 <79865373+TDD66@users.noreply.github.com>
2023-04-05 13:34:58 +0100

    Update V_C.ino

--- V_C.ino
+++ V_C.ino
@@ -1,9 +1,16 @@
-// This is code the will sense current and voltage around a PWM period of 200ms
-#define Current_In_Pin A0
+// This is code the will sense ac current and voltage around a PWM period of 200ms
+// with LED strip integrated
+#include <FastLED.h>
+
+#define Current_In_Pin A5
 #define Voltage_In_Pin A1
 #define PWM_Pin 2
+#define LED_PIN 8
+#define NUM_LEDS 60
+CRGB leds[NUM_LEDS];
+
 int sampling_period  = 200;
-float duty_cycle  = 1;
+float duty_cycle  = 0.5;
 int duration  = 5;
 float t_on = duty_cycle * sampling_period;
 float t_off = sampling_period - t_on;
@@ -26,12 +33,12 @@ float SampleCurrent(int duration) {
     adc_value = analogRead(Current_In_Pin);
     adc_voltage = adc_value * reference_voltage / 1024;
     primary_current = (nominal_current * (adc_voltage - transducer_ref)) / 0.625;
-    total += primary_current;
+    total += primary_current; 
     delay(sample_time);
     endtime = millis();
   }
 
-  float avg_current = (2.05 * total) / samples;
+  float avg_current = total / samples;
   return avg_current;
 }
 
@@ -55,12 +62,13 @@ float SampleVoltage(int duration) {
     endtime = millis();
   }
 
-  float avg_voltage = (1.85 * total) / samples;
+  float avg_voltage = (2 * total) / samples;
   return avg_voltage;
 }
 
 void setup() {
   // put your setup code here, to run once:
+  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
   pinMode(PWM_Pin, OUTPUT);
   digitalWrite(PWM_Pin, HIGH);
 
@@ -73,6 +81,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  FastLED.clear();
   float total_v = 0; // variable to store the running total for voltage for the duty cycle
   float total_c = 0; //
   digitalWrite(PWM_Pin, HIGH);
@@ -89,12 +98,26 @@ void loop() {
 
   digitalWrite(PWM_Pin, LOW);
 
-  float avg_voltage = total_v / 20;
+  float avg_voltage = duty_cycle * (total_v / (t_on/(duration * 2)));
+
   Serial.print("V");
   Serial.println(avg_voltage, 5); // send voltage to python averaged over the 200 ms period
 
-  float avg_current = total_c / 20;
+  int val = map(avg_voltage, 0, 150, 0, NUM_LEDS);
+
+  for(int i = 0; i < val; i++){
+
+    leds[i] = CRGB::Blue;
+    FastLED.show();
+
+  }
+
+  float avg_current = duty_cycle * (total_c / (t_on/(duration * 2)));
   Serial.print("C");
   Serial.println(avg_current, 5);
+
+  float inst_power = avg_current * avg_voltage;
+  Serial.print("P");
+  Serial.println(inst_power, 5);
 
 }
